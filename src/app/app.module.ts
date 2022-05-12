5225bc27a8359690e9b0785907b4a3ecc4c0e681
Giuseppe Marchesiello <juzeppemar90@gmail.com>
2022-05-12 11:40:27 +0200

    assignment-6 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroCardComponent } from './components/hero-card.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HeroCardComponent } from './components/hero-card.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/todo-repository.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '../models/todo';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoRepositoryService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Todo[]> {
+    return this.http.get<Todo[]>('http://localhost:3000/todos');
+  }
+
+  getById(id: number): Observable<Todo> {
+    return this.http.get<Todo>(`http://localhost:3000/todos/${id}`);
+  }
+
+  create(todo: Todo): Observable<Todo> {
+    return this.http.post<Todo>('http://localhost:3000/todos', todo);
+  }
+
+  update(id: number, todo: Todo): Observable<Todo> {
+    return this.http.put<Todo>(`http://localhost:3000/todos/${id}`, todo);
+  }
+
+  deleteById(id: number): Observable<Todo> {
+    return this.http.delete<Todo>(`http://localhost:3000/todos/${id}`);
+  }
+
+}
